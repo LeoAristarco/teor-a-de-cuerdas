4550b23ae0f7491d55b92211957613967e82b7d5
LeoAristarco <leoceit@gmail.com>
2018-03-02 09:59:05 -0300

    Update and rename Producto.java to Bebida.java

---
+++ Bebida.java
@@ -0,0 +1,17 @@
+package somospnt;
+
+public class Bebida extends Producto {
+
+	private double litros;
+
+	public Bebida(String nombre, int precio, double litros) {
+		super(nombre, precio);
+		this.litros = litros;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Litros: " + litros + " /// " + "Precio: $" + getPrecio();
+	}
+
+}
