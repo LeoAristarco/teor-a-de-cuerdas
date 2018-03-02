88dea2a57c887a53b36211035bcd2550efac60c4
LeoAristarco <leoceit@gmail.com>
2018-03-02 09:57:39 -0300

    Create Supermercado.java

---
+++ Supermercado.java
@@ -0,0 +1,58 @@
+package somospnt;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Supermercado {
+
+	private List<Producto> productos;
+
+	public Supermercado() {
+		super();
+		this.productos = new ArrayList<Producto>();
+	}
+
+	public void cargarProducto(Producto unProducto) {
+		productos.add(unProducto);
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void mostrarInformacionDeProductosPorPantalla() {
+		productos.forEach(unProducto -> System.out.println(unProducto.toString()));
+	}
+
+	public void mostrarProductoMasCaroPorPantalla() {
+		ordenarProductosAscendentePorPrecio();
+		System.out.println("Producto más caro: " + getProductoMasCaro().getNombre());
+	}
+
+	public Producto getProductoMasCaro() {
+		return productos.get(ultimoProductoDeLaLista());
+	}
+
+	public int ultimoProductoDeLaLista() {
+		return productos.size() - 1;
+	}
+
+	public void mostrarProductoMasBaratoPorPantalla() {
+		ordenarProductosAscendentePorPrecio();
+		System.out.println("Producto más barato: " + getProductoMasBarato().getNombre());
+	}
+
+	public Producto getProductoMasBarato() {
+		return productos.get(0);
+	}
+
+	public void ordenarProductosAscendentePorPrecio() {
+		Collections.sort(productos, (p1, p2) -> p1.getPrecio() - p2.getPrecio());
+	}
+
+	public void dibujarUnSeparadorGrafico() {
+		System.out.println(("============================="));
+	}
+
+}
